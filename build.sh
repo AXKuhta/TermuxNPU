df91bd39895d0b7276e3840e89a8a2d51be37795
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2022-08-26 22:30:52 +0800

    Update for Android 11

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gcc -lneuralnetworks -Iheaders/ -std=c99 -O3 -Wall -Wextra -Wpedantic -o result.elf list_devices.c
+gcc -lneuralnetworks -L/apex/com.android.neuralnetworks/lib64/ -Iheaders/ -std=c99 -O3 -Wall -Wextra -Wpedantic -o result.elf list_devices.c
 # Add -v to debug includes and linker
